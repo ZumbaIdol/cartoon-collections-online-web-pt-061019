c0952a68f44427f1b684f49ddc85f602c11d3423
David Foote <david.a.foote@gmail.com>
2019-06-14 03:22:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,8 @@ end
 
 def find_the_cheese(cheese_types)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    cheese_types.include?("#{cheese}")
+  cheese_types.find do |type| 
+    cheese_types.include?(type)
+    end
+  end
 end
